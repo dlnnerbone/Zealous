302eb1a279a9edf7ddcd15f27019c9c97e387de0
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-12-19 15:36:16 -0500

    adding gamecomponents to SLN, renaming main Zealous game file, including gamecomponents into project reference via .csproj fileand testing for build issues.

--- Zealous/Program.cs
+++ Zealous/Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new Zealous.Game1();
+﻿using var game = new Zealous.ZealousGame("Zealous");
 game.Run();
---
+++ Zealous/ZealousGame.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Zealous;
+
+public class ZealousGame : Game
+{
+    private GraphicsDeviceManager device;
+    private SpriteBatch spriteBatch;
+    
+    public ZealousGame(string name) 
+    {
+        device = new(this);
+        IsMouseVisible = true;
+        Content.RootDirectory = "Content";
+    }
+    
+    protected override void Initialize() 
+    {
+        base.Initialize();
+        
+        device.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+        device.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
+        device.ApplyChanges();
+    }
+    
+    protected override void LoadContent() 
+    {
+        base.LoadContent();
+        
+        spriteBatch = new(GraphicsDevice);
+    }
+    
+    protected override void Update(GameTime gameTime) 
+    {
+        base.Update(gameTime);
+    }
+    
+    protected override void Draw(GameTime gameTime) 
+    {
+        GraphicsDevice.Clear(Color.Transparent);
+        
+        spriteBatch.Begin();
+        spriteBatch.End();
+        
+        base.Draw(gameTime);
+    }
+}
